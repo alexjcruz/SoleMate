cf6a3d64fead24927fd133511ddcd4ac28be4cbf
Alex Cruz <alexcruz@MacBook-Air.local>
2025-07-21 01:42:49 -0400

    Added Login Screen

---
+++ lib/screens/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._(); 
+
+  static const Color background = Color(0xFFF9FAFB); 
+  static const Color primary = Color(0xFF1F2937);   
+  static const Color textbox = Color(0xFFFFFFFF);     
+  static const Color textplaceholder = Color(0xFFD9D9D9);    
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'home.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // SoleMate
+                Text(
+                  'SoleMate',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: AppColors.primary,
+                  ),
+                ),
+
+                // Track you sneaker collection!
+                SizedBox(height: 5),
+                Text(
+                  'Track your sneaker collection!',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: AppColors.primary,
+                  ),
+                ),
+
+                // Email TextField
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.textbox,
+                      border: Border.all(color: AppColors.primary, width: 1),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Email',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Password TextField
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.textbox,
+                      border: Border.all(color: AppColors.primary, width: 1),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Password',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primary,
+                    padding: const EdgeInsets.symmetric(horizontal: 140.0, vertical: 15.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Home()),
+                    );
+                  }, 
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: AppColors.textbox,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                
+                /*
+                // Dont have an account? Sign Up
+                SizedBox(height: 20),
+                Text('Don\'t have an account? Sign Up',
+                  style: TextStyle(
+                    
+                    fontSize: 16,
+                    color: AppColors.primary,
+                  ),
+                ),
+                */
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+  }
